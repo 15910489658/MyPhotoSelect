99b00769d132ffcc7ab6a956fa28fb45108b8342
Admin <15910489658@163.com>
2019-10-17 15:01:02 +0800

    初始化

---
+++ myphotolib/src/main/java/com/DefaultCompany/ProductName/core/ImageLoaderHelper.java
@@ -0,0 +1,69 @@
+package com.DefaultCompany.ProductName.core;
+
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+
+
+/**
+ * 　　　　　　　　┏┓　　　┏┓
+ * 　　　　　　　┏┛┻━━━┛┻┓
+ * 　　　　　　　┃　　　　　　　┃
+ * 　　　　　　　┃　　　━　　　┃
+ * 　　　　　　 ████━████     ┃
+ * 　　　　　　　┃　　　　　　　┃
+ * 　　　　　　　┃　　　┻　　　┃
+ * 　　　　　　　┃　　　　　　　┃
+ * 　　　　　　　┗━┓　　　┏━┛
+ * 　　　　　　　　　┃　　　┃
+ * 　　　　　　　　　┃　　　┃
+ * 　　　　　　　　　┃　　　┃
+ * 　　　　　　　　　┃　　　┃
+ * 　　　　　　　　　┃　　　┃
+ * 　　　　　　　　　┃　　　┃
+ * 　　　　　　　　　┃　 　 ┗━━━┓
+ * 　　　　　　　　　┃ 神兽保佑　　 ┣┓
+ * 　　　　　　　　　┃ 代码无BUG   ┏┛
+ * 　　　　　　　　　┗┓┓┏━┳┓┏┛
+ * 　　　　　　　　　　┃┫┫　┃┫┫
+ * 　　　　　　　　　　┗┻┛　┗┻┛
+ *
+ * 图片加载ImageLoader 的option配置
+ */
+public class ImageLoaderHelper {
+
+    
+    /**
+     * <li> 默认option配置 </li>,可设置默认显示图片
+     */
+    public static DisplayImageOptions buildDisplayImageOptionsDefault(int drawable) {
+    	BitmapFactory.Options options = new BitmapFactory.Options();
+		options.inSampleSize = 1024*2;
+        return new DisplayImageOptions.Builder()
+        		.showImageOnLoading(drawable)
+        		.showImageForEmptyUri(drawable)
+        		.showImageOnFail(drawable)
+        		.decodingOptions(options)
+                .cacheInMemory(true)
+                .cacheOnDisk(true)
+                .bitmapConfig(Bitmap.Config.RGB_565)
+                .build();
+    }
+
+	/**
+	 * <li> 默认option配置 </li>
+	 */
+	public static DisplayImageOptions buildDisplayImageOptionsPager() {
+		BitmapFactory.Options options = new BitmapFactory.Options();
+		options.inSampleSize = 4;
+		return new DisplayImageOptions.Builder()
+				.decodingOptions(options)
+				.cacheInMemory(true)
+				.cacheOnDisk(true)
+				.bitmapConfig(Bitmap.Config.RGB_565)
+				.build();
+	}
+
+}
