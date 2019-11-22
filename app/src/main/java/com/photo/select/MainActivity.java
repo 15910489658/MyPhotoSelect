f27948bc58d998c400bbc049771a17b2850c683e
JulyJiangs <15910489658@163.com>
2019-11-22 17:29:45 +0800

    配置全局gradle

--- app/src/main/java/com/photo/select/MainActivity.java
+++ app/src/main/java/com/photo/select/MainActivity.java
@@ -6,7 +6,6 @@ import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
